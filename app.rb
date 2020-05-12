f1d897e169a7507c978936572b72e58d7c2c1719
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-12 13:23:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,16 +12,16 @@ class App < Sinatra::Base
     "#{@result}"
   end
 
-  # get "/say/:number/:phrase" do
-  #   @string = ""
-  #   @number = params[:number].to_i
-  #   @phrase = params[:phrase]
-  #   @number.times do
-  #     @the_product += @the_phrase
-  #     @the_product += "\n"
-  #   end
-  #   "#{@the_product}"
-  # end
+  get "/say/:number/:phrase" do
+    @string = ""
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @number.times do
+      @the_product += @the_phrase
+      @the_product += "\n"
+    end
+    "#{@the_product}"
+  end
 
   # get "/say/:word1/:word2/:word3/:word4/:word5" do
   #   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
