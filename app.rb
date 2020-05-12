a8ef9b04ed05551a551147712009cd5b65272f11
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-12 13:24:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,18 +27,18 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  # get "/:operation/:number1/:number2" do
-  #   @number_1 = params[:number1].to_i
-  #   @number_2 = params[:number2].to_i
-  #   if params[:operation] == "add"
-  #     "#{@number_1+@number_2}"
-  #   elsif params[:operation] == "subtract"
-  #     "#{@number_1-@number_2}"
-  #   elsif params[:operation] == "multiply"
-  #     "#{@number_1*@number_2}"
-  #   elsif params[:operation] == "divide"
-  #     "#{@number_1/@number_2}"
-  #   end
-  # end
+  get "/:operation/:number1/:number2" do
+    @number_1 = params[:number1].to_i
+    @number_2 = params[:number2].to_i
+    if params[:operation] == "add"
+      "#{@number_1+@number_2}"
+    elsif params[:operation] == "subtract"
+      "#{@number_1-@number_2}"
+    elsif params[:operation] == "multiply"
+      "#{@number_1*@number_2}"
+    elsif params[:operation] == "divide"
+      "#{@number_1/@number_2}"
+    end
+  end
 
 end
